2225d4a44edb0e39d9a26bfb322b1d1986639ce5
sruthi <sruthi.gollaplly26>
2024-04-04 23:01:14 +0530

    first commit

---
+++ Calci_test.java
@@ -0,0 +1,11 @@
+package Calulator;
+import static.org.junit.jupiter.api.assertions.*;
+import org.juint.jupiter.api.Test;
+
+public class Calci_test {
+    @Test
+     public void TestSum() {
+	
+}
+
+}
